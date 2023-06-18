a5b0cdeeadf7984044cc294293983e9dd062bbb1
Alesandro Zuzic <azuzic@student.unipu.hr>
2023-06-18 07:31:03 +0200

    small input fix

--- src/components/ContactUs/BN_FileInput.vue
+++ src/components/ContactUs/BN_FileInput.vue
@@ -3,7 +3,7 @@
         <div class="w-1/2 h-24 focus:border-BN_PRIMARY border-dashed hover:border-BN_BACKGROUND2 border-BN_BACKGROUND border-4 rounded-xl 
             transition-all relative flex justify-center items-center">
             <input type="file" class="w-full h-full text-3xl p-4 peer cursor-pointer">
-            <div class="absolute text-2xl font-bold text-BN_FONT peer-hover:underline hover:underline cursor-pointer"><i class="fa-solid fa-paperclip mr-2"></i>Attach files</div>
+            <div class="absolute text-2xl font-bold text-BN_FONT peer-hover:underline hover:underline cursor-pointer pointer-events-none"><i class="fa-solid fa-paperclip mr-2"></i>Attach files</div>
         </div>
         
         <div class="absolute text-BN_FONT peer-focus:-top-8 peer-focus:left-0 peer-focus:text-base transition-all pointer-events-none"
